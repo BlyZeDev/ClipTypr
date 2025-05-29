be41b0a2058f24088d381a5dd59ff4b0121afaa6
BlyZeDev <leonschimmel15@gmail.com>
2025-05-29 02:48:31 +0200

    Implemented Pico file handling functionality. Maybe also add handshake if needed? Prepared C# Implementation

--- ClipTypr_Pico/hid/hid.ino
+++ ClipTypr_Pico/hid/hid.ino
@@ -1,31 +1,71 @@
 #include "PluggableUSBHID.h"
 #include "USBKeyboard.h"
 
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
-
-typedef int8_t s8;
-typedef int16_t s16;
-typedef int32_t s32;
-typedef int64_t s64;
+const char* Token = "FF302DB2-AFCB-4EC2-93DE-87A6EABF47CC";
+const size_t TokenLength = strlen(Token);
+const char Separator = '|';
+const unsigned long BlinkDuration = 300;
 
 USBKeyboard Keyboard;
+bool isHandlingStream;
+unsigned long ledOnTime = 0;
+
+void processChunk(String chunk)
+{
+    while (true)
+    {
+        int tokenPos = chunk.indexOf(Token);
+        if (tokenPos < 0)
+        {
+            if (isHandlingStream)
+            {
+                ledOnTime = millis();
+                digitalWrite(LED_BUILTIN, HIGH);
+
+                Keyboard.printf("%s", chunk.c_str());
+            }
+            break;
+        }
+
+        if (tokenPos > 0 && isHandlingStream)
+        {
+            ledOnTime = millis();
+            digitalWrite(LED_BUILTIN, HIGH);
+
+            String beforeToken = chunk.substring(0, tokenPos);
+            Keyboard.printf("%s", beforeToken.c_str());
+        }
+
+        isHandlingStream = !isHandlingStream;
+
+        chunk = chunk.substring(tokenPos + TokenLength);
+    }
+}
 
 void setup()
 {
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+
     Serial.begin(115200);
     while (!Serial) delay(10);
-    Serial.println("Setup complete");
+    isHandlingStream = false;
 }
 
 void loop()
 {
     if (Serial.available())
     {
-        Serial.println("Printing...");
-        Keyboard.printf("Hello world\n");
-        delay(1000);
+        String chunk = Serial.readStringUntil(Separator);
+        processChunk(chunk);
+        digitalWrite(LED_BUILTIN, LOW);
+    }
+
+    if (digitalRead(LED_BUILTIN) == HIGH)
+    {
+        if (millis() - ledOnTime >= BlinkDuration)
+        {
+            digitalWrite(LED_BUILTIN, LOW);
+        }
     }
 }
