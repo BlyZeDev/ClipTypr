df7e7a07bfa708153cc0f27a6bf98fdf5d44c74c
BlyZeDev <leonschimmel15@gmail.com>
2025-07-10 21:30:13 +0200

    Fixed typos

--- ClipTypr/Common/Util.cs
+++ ClipTypr/Common/Util.cs
@@ -14,9 +14,9 @@ public static class Util
             var _ when timeSpan.TotalHours >= 1 => (timeSpan.TotalHours, "hour"),
             var _ when timeSpan.TotalMinutes >= 1 => (timeSpan.TotalMinutes, "minute"),
             var _ when timeSpan.TotalSeconds >= 1 => (timeSpan.TotalSeconds, "second"),
-            var _ when timeSpan.TotalMilliseconds >= 1 => (timeSpan.TotalMilliseconds, "milliseconds"),
-            var _ when timeSpan.TotalMicroseconds >= 1 => (timeSpan.TotalMicroseconds, "microseconds"),
-            _ => (timeSpan.TotalNanoseconds, "nanoseconds")
+            var _ when timeSpan.TotalMilliseconds >= 1 => (timeSpan.TotalMilliseconds, "millisecond"),
+            var _ when timeSpan.TotalMicroseconds >= 1 => (timeSpan.TotalMicroseconds, "microsecond"),
+            _ => (timeSpan.TotalNanoseconds, "nanosecond")
         };
 
         if (value != 1) unit += 's';
